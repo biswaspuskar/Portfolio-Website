ca3018a300b6668f4658d1cd7846983c9c2f4a70
biswaspuskar <140431075+biswaspuskar@users.noreply.github.com>
2023-08-06 21:27:56 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,115 @@
+var typed=new Typed(".changing",{
+    strings:["Front-end Developer","Student","Designer","Photographer"],
+    typedSpeed:50,
+    backSpeed:50,
+    loop:true,
+})
+
+
+/*-------------------------------------------------------------------------*/
+
+// var darkBtn = document.getElementById("dark");
+// var lightBtn = document.getElementById("light");
+// var colorB = document.getElementById("color");
+// var ar = document.getElementsByClassName("logo-text")[0]; 
+// var list = document.querySelectorAll(".navlinks li a");
+// var hi=document.querySelector("h5");
+// var pg=document.getElementById("para")
+// var social = document.querySelectorAll(".hero-text .social i");
+
+// darkBtn.addEventListener("click", function() {
+//     if (colorB.style.backgroundColor === "black" || colorB.style.backgroundColor === "") {
+//         colorB.style.backgroundColor = "white";
+//         darkBtn.style.color = "black";
+//         ar.style.color = "black";
+//         hi.style.color="black"
+//         pg.style.color="black";
+        
+
+//         for (var i = 0; i < list.length; i++) {
+//             list[i].style.color = "black";
+//           }
+      
+//           // Loop through each social icon and change the color to black
+//           for (var i = 0; i < social.length; i++) {
+//             social[i].style.color = "black";
+//           }
+
+//         } else {
+//           colorB.style.backgroundColor = "black";
+//           darkBtn.style.color = "white";
+//           ar.style.color = "white";
+//           hi.style.color = "white";
+//           pg.style.color = "white";
+          
+      
+//           // Loop through each anchor element and change the text color to white
+//           for (var i = 0; i < list.length; i++) {
+//             list[i].style.color = "white";
+//           }
+      
+//           // Loop through each social icon and change the color to white
+//           for (var i = 0; i < social.length; i++) {
+//             social[i].style.color = "white";
+//           }
+          
+//         }
+//       });
+
+
+// var darkBtn = document.getElementById("dark");
+// var lightBtn = document.getElementById("light");
+// var colorB = document.getElementById("color");
+// var ar = document.getElementsByClassName("logo-text")[0];
+// var list = document.querySelectorAll(".navlinks li a");
+// var hi = document.querySelector("h5");
+// var pg = document.getElementById("para");
+// var social = document.querySelectorAll(".hero-text .social i");
+
+// darkBtn.addEventListener("click", function () {
+//   var currentBgColor = colorB.style.backgroundColor || window.getComputedStyle(colorB).backgroundColor;
+//   var isDarkMode = currentBgColor === "black";
+
+//   if (isDarkMode) {
+//     // Switch to light mode
+//     colorB.style.backgroundColor = "white";
+//     darkBtn.style.display = "inline";
+//     lightBtn.style.display = "none";
+//     ar.style.color = "black";
+//     hi.style.color = "black";
+//     pg.style.color = "black";
+
+//     // Loop through each anchor element and change the text color to black
+//     for (var i = 0; i < list.length; i++) {
+//       list[i].style.color = "black";
+//     }
+
+//     // Loop through each social icon and change the color to black
+//     for (var i = 0; i < social.length; i++) {
+//       social[i].style.color = "black";
+//     }
+//   } else {
+//     // Switch to dark mode
+//     colorB.style.backgroundColor = "black";
+//     darkBtn.style.display = "none";
+//     lightBtn.style.display = "inline";
+//     ar.style.color = "white";
+//     hi.style.color = "white";
+//     pg.style.color = "white";
+
+//     // Loop through each anchor element and change the text color to white
+//     for (var i = 0; i < list.length; i++) {
+//       list[i].style.color = "white";
+//     }
+
+//     // Loop through each social icon and change the color to white
+//     for (var i = 0; i < social.length; i++) {
+//       social[i].style.color = "white";
+//     }
+//   }
+// });
+
+// // Initialize the page with the appropriate mode icon (assumes dark mode initially)
+// lightBtn.style.display = "none";
+// darkBtn.style.display = "inline";
+
